7dd4a7f15cb1f3323749e1da329b03dde076dc9c
abar <alexandre.bar.france@gmail.com>
2013-03-12 15:41:24 +0100

    essais infructueux

--- Kinect_Architecture/MainWindow.xaml.cs
+++ Kinect_Architecture/MainWindow.xaml.cs
@@ -17,84 +17,48 @@ using System.Windows.Shapes;
 using Coding4Fun.Kinect.Wpf.Controls;
 
 
-namespace Kinect
+namespace Kinect_Architecture
 {
     /// <summary>
     /// Logique d'interaction pour MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-
-        /* Variables */
-        private KinectMain kinect;
-       
-
-        public MainWindow()
-        {
-            InitializeComponent();
-            kinect = new KinectMain(global, kinectButton, button1, button2, quitButton);
-            kinectButton.Click += new RoutedEventHandler(this.kinect.curseur.kinectButton_Click);
-            
-        }
 
 
 
-        ////When the window is loaded
-        private void Window_Loaded(Object sender, RoutedEventArgs e)
-        {
-            kinect.InitKinect(StickMen);
-        }
 
 
-        // Methode pour un zoom/dezoom 'fluide'
 
-        //private bool ZoomDezoom(Skeleton skeleton)
-        //{
+    // Methode pour un zoom/dezoom 'fluide'
 
-        //    CoordinateMapper cmLeft = new CoordinateMapper(this.sensor);
-        //    CoordinateMapper cmRight = new CoordinateMapper(this.sensor);
-        //    ColorImagePoint Left = cmLeft.MapSkeletonPointToColorPoint(skeleton.Joints[JointType.HandLeft].Position, ColorImageFormat.RgbResolution1280x960Fps12);
-        //    ColorImagePoint Right = cmRight.MapSkeletonPointToColorPoint(skeleton.Joints[JointType.HandRight].Position, ColorImageFormat.RgbResolution1280x960Fps12);
-        //    // // Coordonnée en Z proche 
-        //    //  if ((skeleton.Joints[JointType.HandRight].Position.Z <= skeleton.Joints[JointType.HandLeft].Position.Z + 1 && skeleton.Joints[JointType.HandRight].Position.Z >= skeleton.Joints[JointType.HandLeft].Position.Z - 1))
-        //    //  {
-        //    // Coordonnée en Y proche 
-        //    if ((Right.Y <= Left.Y + 40 && Right.Y >= Left.Y - 40)) // rajouté une condition sur la position par rapport au corps
-        //    {
-        //        // Coordonnée en X 'centré'
-        //        if (Right.X - 1280 / 2 < 1280 / 2 - Left.X + 40 && Right.X - 1280 / 2 > 1280 / 2 - Left.X - 40)
-        //        {
-        //            xZoomL = Left.X;
-        //            xZoomR = Right.X;
+    //private bool ZoomDezoom(Skeleton skeleton)
+    //{
 
-        //            Canvas.SetLeft(rectZoom, 2 * Left.X);
-        //            rectZoom.Width = 2 * Right.X - 2 * Left.X;
-        //            return true;
-        //        }
+    //    CoordinateMapper cmLeft = new CoordinateMapper(this.sensor);
+    //    CoordinateMapper cmRight = new CoordinateMapper(this.sensor);
+    //    ColorImagePoint Left = cmLeft.MapSkeletonPointToColorPoint(skeleton.Joints[JointType.HandLeft].Position, ColorImageFormat.RgbResolution1280x960Fps12);
+    //    ColorImagePoint Right = cmRight.MapSkeletonPointToColorPoint(skeleton.Joints[JointType.HandRight].Position, ColorImageFormat.RgbResolution1280x960Fps12);
+    //    // // Coordonnée en Z proche 
+    //    //  if ((skeleton.Joints[JointType.HandRight].Position.Z <= skeleton.Joints[JointType.HandLeft].Position.Z + 1 && skeleton.Joints[JointType.HandRight].Position.Z >= skeleton.Joints[JointType.HandLeft].Position.Z - 1))
+    //    //  {
+    //    // Coordonnée en Y proche 
+    //    if ((Right.Y <= Left.Y + 40 && Right.Y >= Left.Y - 40)) // rajouté une condition sur la position par rapport au corps
+    //    {
+    //        // Coordonnée en X 'centré'
+    //        if (Right.X - 1280 / 2 < 1280 / 2 - Left.X + 40 && Right.X - 1280 / 2 > 1280 / 2 - Left.X - 40)
+    //        {
+    //            xZoomL = Left.X;
+    //            xZoomR = Right.X;
 
-        //    }
+    //            Canvas.SetLeft(rectZoom, 2 * Left.X);
+    //            rectZoom.Width = 2 * Right.X - 2 * Left.X;
+    //            return true;
+    //        }
 
-        //    xZoomL = -1;
-        //    xZoomR = -1;
-        //    return false;
+    //    }
 
-        //}
+    //    xZoomL = -1;
+    //    xZoomR = -1;
+    //    return false;
 
-        public void button1_Click(object sender, RoutedEventArgs e)
-        {
-            System.Console.WriteLine("Bouton 1 clické");
-            message.Content = "Button 1 clicked!";
-        }
-
-        public void button2_Click(object sender, RoutedEventArgs e)
-        {
-            System.Console.WriteLine("Bouton 2 clické");
-            message.Content = "Button 2 clicked!";
-        }
-
-        public void quitButton_Click(object sender, RoutedEventArgs e)
-        {
-            System.Windows.Application.Current.Shutdown();
-        }
-    }
+    //}
 }
